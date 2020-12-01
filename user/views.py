ab01a0b43da83aa6681b9e37aa06b46eb63136e7
İsmail Sefa <ismailsefa.akdnz@gmail.com>
2020-12-02 01:39:28 +0300

    create templates folder and user html files

--- user/views.py
+++ user/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def login(request):
+    return render(request,'user/login.html')
+
+def register(request):
+    return render(request,'user/register.html')
+
+def logout():
+    pass
