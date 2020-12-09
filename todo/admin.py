62dd4d34cafd5a3b043f6189dbfa838e89939b2d
İsmail Sefa <ismailsefa.akdnz@gmail.com>
2020-12-09 15:57:10 +0300

    created list model and adding to admin panel

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
+
+class ListAdmin(admin.ModelAdmin):
+    list_display = ('listName','user')
+    list_display_links = ('listName','user')
+    list_filter = ('listName','listName')
+    list_per_page = 10
 
 # Register your models here.
+
+admin.site.register(models.List,ListAdmin)
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class List(models.Model):
+    listName = models.CharField(max_length=100,verbose_name="List Name")
+    user = models.ForeignKey("auth.User", on_delete=models.CASCADE,verbose_name='User')
+
+    def __str__(self):
+        return self.listName
+
+    class Meta:
+        verbose_name = 'Todo List'
+        verbose_name_plural = 'Todo Lists'
