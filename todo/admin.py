f257a40fd9686f9b5d7e7e291a0c54878e23d3c1
İsmail Sefa <ismailsefa.akdnz@gmail.com>
2020-12-09 16:27:18 +0300

    created list_item model and adding to admin panel

--- todo/admin.py
+++ todo/admin.py
@@ -2,11 +2,18 @@ from django.contrib import admin
 from . import models
 
 class ListAdmin(admin.ModelAdmin):
-    list_display = ('listName','user')
-    list_display_links = ('listName','user')
-    list_filter = ('listName','listName')
+    list_display = ('name','user')
+    list_display_links = ('name','user')
+    list_filter = ('name','user')
+    list_per_page = 10
+
+class ListItemAdmin(admin.ModelAdmin):
+    list_display = ('name','deadline','createdDate','status')
+    list_display_links = ('name','deadline','createdDate','status')
+    list_filter = ('name','deadline','createdDate','status')
     list_per_page = 10
 
 # Register your models here.
 
 admin.site.register(models.List,ListAdmin)
+admin.site.register(models.ListItem,ListItemAdmin)
--- todo/models.py
+++ todo/models.py
@@ -3,12 +3,27 @@ from django.db import models
 # Create your models here.
 
 class List(models.Model):
-    listName = models.CharField(max_length=100,verbose_name="List Name")
+    name = models.CharField(max_length=100,verbose_name="List Name")
     user = models.ForeignKey("auth.User", on_delete=models.CASCADE,verbose_name='User')
 
     def __str__(self):
-        return self.listName
+        return self.name
 
     class Meta:
         verbose_name = 'Todo List'
         verbose_name_plural = 'Todo Lists'
+
+class ListItem(models.Model):
+    name = models.CharField(max_length=100,verbose_name="Item Name")
+    explanation = models.CharField(max_length=100,verbose_name="Item Explanation")
+    deadline = models.DateTimeField(verbose_name='Item Deadline')
+    createdDate = models.DateTimeField(auto_now_add=True, verbose_name="Item Created Date")
+    status = models.BooleanField(verbose_name='is Complete?')
+    tList = models.ForeignKey(List, on_delete=models.CASCADE,verbose_name='List')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Todo List Item'
+        verbose_name_plural = 'Todo List Items'
