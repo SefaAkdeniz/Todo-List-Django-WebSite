102e660ad39d78d365751be2e28e1e9e730ee1cd
İsmail Sefa <ismailsefa.akdnz@gmail.com>
2020-12-13 00:01:13 +0300

    added list detail page

--- todo/views.py
+++ todo/views.py
@@ -9,3 +9,8 @@ def index(request):
     lists = List.objects.filter(user=request.user)
     return render(request,'index.html',{"lists": lists})
     
+@login_required(login_url="login") 
+def detail(request,list_id):
+    items = ListItem.objects.filter(tList=list_id)
+    title = List.objects.filter(pk=list_id).first().name
+    return render(request,'detail.html',{"items": items,"title":title})
