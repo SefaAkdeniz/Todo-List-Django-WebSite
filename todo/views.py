e527247cda2fafed3389ae4a504047f49d14cba5
İsmail Sefa <ismailsefa.akdnz@gmail.com>
2020-12-13 15:26:08 +0300

    added delete list operation

--- todo/views.py
+++ todo/views.py
@@ -22,3 +22,8 @@ def detail(request,list_id):
     items = ListItem.objects.filter(tList=list_id)
     title = List.objects.filter(pk=list_id).first().name
     return render(request,'detail.html',{"items": items,"title":title})
+
+@login_required(login_url="login") 
+def addList(request,list_id):
+    List.objects.filter(pk=list_id).delete()
+    return redirect('index')
